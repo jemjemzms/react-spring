39ed971e2963c5b9b2ed69c66e66bc40a9b55670
Jed Diaz <jemjemzms@gmail.com>
2019-01-14 23:13:56 +1300

    Adding initial files
    Adding initial files

---
+++ src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+      <p>Lorem ipsum dolor sit, amet consectur adipsicing elit. Possimus nobis adipisci eum minima deserunt at porr.</p>
+    </div>
+  )
+}
+
+const c3Style = {
+  background: 'skyblue',
+  color: 'white',
+  padding: '1.5rem'
+}
